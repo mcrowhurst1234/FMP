d5951fbddb7678c8c6c45d7beadae20ca63d12a6
Navr0n <95281268+Navr0n@users.noreply.github.com>
2022-03-24 12:36:59 +0000

    item stack

--- Assets/Scripts/Inventory System/Item.cs
+++ Assets/Scripts/Inventory System/Item.cs
@@ -2,17 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item : MonoBehaviour
+[CreateAssetMenu(fileName = "New Item")]
+public class Item : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string ItemName;
+    public Sprite ItemIcon;
+    [Range(1, 100)]public int maxStackSize = 100; 
 }
