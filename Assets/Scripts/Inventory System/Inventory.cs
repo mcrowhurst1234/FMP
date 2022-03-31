cf73a83224c9a733e445907f57c20bfb9d6e066c
Navr0n <95281268+Navr0n@users.noreply.github.com>
2022-03-31 12:43:02 +0100

    ee

---
+++ Assets/Scripts/Inventory System/Inventory.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory 
+{
+    private List<ItemStack> inventoryContents = new List<ItemStack>();
+
+    public Inventory(int size)
+    {
+        for(int i = 0; i < size; i++)
+        {
+            inventoryContents.Add(new ItemStack(i));
+        }
+    }
+
+    public bool addItem(ItemStack input)
+    {
+        foreach(ItemStack stack in inventoryContents)
+        {
+            if(stack.isEmpty())//Itemstack has no item
+            {
+                stack.setStack(input);
+                return true;
+            }
+            else//Itemstack has an item 
+            {
+                if(ItemStack.areItemsEqual(input, stack))
+                {
+                    if(stack.canAddToo(input.get)))
+                    {
+                        stack.increaseAmount(input.getCount());
+                        return true;
+                    }
+                    else
+                    {
+                        int difference = (stack.getCount() + input.getCount()) - stack.getItem().maxStackSize;
+                    }
+                }
+            }
+        }
+    }
+}
