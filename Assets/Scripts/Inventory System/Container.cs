0d18ca8041544f602b17d396aa49f12ce602fbec
Navr0n <chr1st1an0701504@gmail.com>
2022-04-29 00:34:31 +0100

    inventory system

---
+++ Assets/Scripts/Inventory System/Container.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Container : MonoBehaviour
+{
+    private GameObject spawnedContainerPrefab;
+    private Inventory containerInventory;
+    private Inventory playerInventory;
+
+    public Container(Inventory containerInventory, Inventory playerInventory)
+    {
+        this.containerInventory = containerInventory;
+        this.playerInventory = playerInventory;
+        openContainer();
+    }
+
+    public void openContainer()
+    {
+        spawnedContainerPrefab = Object.Instantiate(getContainerPrefab(), InventoryManager.INSTANCE.transform); 
+    }
+
+    public void closeContainer()
+    {
+        Object.Destroy(spawnedContainerPrefab);
+    }
+
+    public virtual GameObject getContainerPrefab()
+    {
+        return null;
+    }
+
+    public GameObject getSpawnedContainer()
+    {
+        return spawnedContainerPrefab;
+    }
+
+    public Inventory getContainerInventory()
+    {
+        return containerInventory;
+    }
+
+    public Inventory getPlayerInventory()
+    {
+        return playerInventory;
+    }
+}
